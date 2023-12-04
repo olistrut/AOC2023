187699afab6fe1dc14b3cf495c9608199f999f76
Oliver Strutynski <34685576+olistrut@users.noreply.github.com>
2023-12-04 12:14:28 +0100

    Add files via upload

---
+++ Day04.py
@@ -0,0 +1,49 @@
+import time
+from collections import defaultdict
+
+def def_value():
+    return 1
+
+def solve(filename):
+    result1 = 0
+    result2 = 0
+
+    f = open(filename)
+    file = open(filename)
+
+    current = 0
+
+    lines = f.readlines()
+    multiplier = [1] * len(lines)
+
+    for blocks in [s.strip().split(":")[1].split("|") for s in lines]:
+        winning = set(blocks[0].split())
+        having = set(blocks[1].split())
+
+        count = len(winning & having)
+
+        for i in range(count):
+            multiplier[current+i+1] += multiplier[current]
+
+        if(count>0):
+            result1 += 2**(count-1)
+        current +=1
+
+    result2 = sum(multiplier)
+    return result1, result2
+
+start = time.time()
+filename = "input/input04-sample.txt"
+p1, p2 = solve(filename)
+print("Part 1 (Example): ", p1)
+print('Part 2 (Example): ', p2)
+
+filename = "input/input04.txt"
+p1, p2 = solve(filename)
+print("Part 1 (Data): ", p1)
+print('Part 2 (Data): ', p2)
+
+print("Total time: ", time.time() - start)
+
+
+
